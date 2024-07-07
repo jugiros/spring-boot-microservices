97874a83e7ed68964ad6dc7a48ec00d30f8688aa
Juan Molina <jmolina@initiumsoft.com>
2024-07-07 12:09:58 -0500

    feat: Primer commit.

---
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/springboot_servicio_productos/SpringbootServicioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.productos.springboot_servicio_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioProductosApplication.class, args);
+	}
+
+}
---
+++ springboot-servicio-productos/src/test/java/com/formacionbdi/springboot/app/productos/springboot_servicio_productos/SpringbootServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.productos.springboot_servicio_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
