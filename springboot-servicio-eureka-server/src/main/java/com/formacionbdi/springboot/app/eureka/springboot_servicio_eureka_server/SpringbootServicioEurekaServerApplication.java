be90a9d52daf36557bcaedf5e5b7125497fae360
Juan Molina <jmolina@initiumsoft.com>
2024-07-09 14:55:10 -0500

    feat: Server Eureka.

--- springboot-servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/springboot_servicio_eureka_server/SpringbootServicioEurekaServerApplication.java
+++ springboot-servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/springboot_servicio_eureka_server/SpringbootServicioEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.formacionbdi.springboot.app.eureka.springboot_servicio_eureka_server
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringbootServicioEurekaServerApplication {
 
